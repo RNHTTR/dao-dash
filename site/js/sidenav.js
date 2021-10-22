8ed5b191191734556b89e278c86b447cdb2549cd
RNHTTR <ryan@wiftapp.com>
2021-10-22 10:50:10 -0400

    removing site from gitignore

---
+++ site/js/sidenav.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+    var instance = M.Sidenav.getInstance(elem);
+    instance.open();
+
+});

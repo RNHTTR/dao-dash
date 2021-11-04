82286312f352ab1e72e2c6c52bb4104732aa6c49
RNHTTR <ryan@wiftapp.com>
2021-11-03 22:02:03 -0400

    connected superset and api

--- site/app.py
+++ site/app.py
@@ -1,4 +1,5 @@
 import os
+from json import JSONDecodeError
 
 import requests
 from flask import Flask, make_response, render_template, request
@@ -6,15 +7,14 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 DAO_ADDRESS = os.getenv("DAO_ADDRESS")
-DAO_ADDRESS = "0x6b175474e89094c44da98b954eedeac495271d0f"
+DAO_ADDRESS = "0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2"
 SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
 API_KEY = os.getenv("API_KEY")
 API_KEY = "ckey_f17f2cba6bad472a97955f7b7da"
 CHAIN_ID = os.getenv("CHAIN_ID")
 CHAIN_ID = 1
 
-BASE_URL = "https://api.covalenthq.com"
-HISTORICAL_VALUE_ENDPOINT = f"v1/{CHAIN_ID}/address/{DAO_ADDRESS}/portfolio_v2"
+BASE_URL = f"https://api.covalenthq.com/v1/{CHAIN_ID}"
 AUTH_QUERY = f"?&key={API_KEY}"
 
 app = Flask(__name__)
@@ -27,24 +27,43 @@ class HistoricalPrice(db.Model):
     quote_rate = db.Column(db.Float, nullable=False)
 
 
-@app.route('/refresh', methods=["POST", "OPTIONS"])
-def refresh():
+@app.route('/refresh/<query>', methods=["POST", "OPTIONS"])
+def refresh(query):
     # TODO: Is this necessary?
     # Review https://stackoverflow.com/a/52875875/7004653
     if request.method == "OPTIONS":
         print("OPTIONS")
         return _build_cors_preflight_response()
     elif request.method == "POST":
-        print("POST")
-        url = f"{BASE_URL}/{HISTORICAL_VALUE_ENDPOINT}/{AUTH_QUERY}"
-        response = requests.get(url)
-        historical_price_data = response.json()["items"][0]["holdings"]
-        quote_rate = historical_price_data[0]["quote_rate"]
-        print(f"QUOTE RATE: {quote_rate}")
-        return {
-            "code": 200,
-            "quote_rate": quote_rate
-        }
+        if query == "token_price":
+            print("POST")
+            url = f"{BASE_URL}/address/{DAO_ADDRESS}/portfolio_v2/{AUTH_QUERY}"
+            response = requests.get(url)
+            try:
+                historical_price_data = response.json()["items"][0]["holdings"]
+            except JSONDecodeError:
+                return {
+                    "code": 500,
+                    "token_price": "Error querying API. Try again with Dashboard -> Refresh"
+                }
+            quote_rate = historical_price_data[0]["quote_rate"]
+            print(f"QUOTE RATE: {quote_rate}")
+            return {
+                "code": 200,
+                "token_price": quote_rate
+            }
+        elif query == "token_holders":
+            print("POST")
+
+            url = f"{BASE_URL}/tokens/{DAO_ADDRESS}/token_holders_changes/" \
+                  f"{AUTH_QUERY}&starting-block=1&page-size=1"
+            response = requests.get(url)
+            token_holders = response \
+                .json()["data"]["pagination"]["total_count"]
+            return {
+                "code": 200,
+                "token_holders": token_holders
+            }
         # return _corsify_actual_response(jsonify(response=200))
     else:
         raise RuntimeError(
--- site/static/js/refresh.js
+++ site/static/js/refresh.js
@@ -1,29 +1,54 @@
-const refresh = async () => {
-    const response = await fetch('http://127.0.0.1:5000/refresh', {
+window.onload = function() {
+    refresh(cache=true);
+    console.log("refreshing...")
+  };
+
+const refresh = async (cache) => {
+    if (cache) {
+        console.log("refreshing!")
+        let tokenPrice = sessionStorage.getItem("tokenPrice");
+        let tokenHolders = sessionStorage.getItem("tokenHolders");
+        console.log(tokenPrice)
+        if ( tokenPrice && tokenHolders ) {
+            document.getElementById("tokenPrice").innerHTML = sessionStorage.getItem("tokenPrice");
+            document.getElementById("tokenHolders").innerHTML = sessionStorage.getItem("tokenHolders");
+            return
+        }
+    }
+    refreshTokenPrice()
+    refreshTokenHolders()
+}
+
+const refreshTokenPrice = async () => {
+    document.getElementById("tokenPrice").innerHTML = '<i class="fa fa-spinner fa-pulse fa-3x fa-fw"></i>'
+    const response = await fetch('http://127.0.0.1:5000/refresh/token_price', {
         method: 'POST',
         body: "",
-        // headers: {
-        //     'Content-Type': 'application/json'
-        // }
     })
     if (response.ok) {
-      const data = await response.json();
-      console.log(data);
-        toastr.success("Data refreshed.", "Success", {
-            timeOut:4000,
-            newestOnTop:!0,
-            progressBar:!0,
-            preventDuplicates:false
-        });
+        const data = await response.json();
+        console.log(data);
+        document.getElementById("tokenPrice").innerHTML = data["token_price"];
+        sessionStorage.setItem("tokenPrice", data["token_price"]);
     } else {
-        toastr.error("Something went wrong.", "Oops!", {
-            timeOut:4000,
-            newestOnTop:!0,
-            progressBar:!0,
-            preventDuplicates:false
-        });
+        document.getElementById("tokenPrice").innerHTML = "Error querying API. Try again with Dashboard -> Refresh"
         console.log(response);
-        document.getElementById("tokenPrice").innerHTML = "LFG"; 
-        document.getElementById("tokenHolders").innerHTML = "LFG!!!";
     }
-  }
+}
+
+const refreshTokenHolders = async () => {
+    document.getElementById("tokenHolders").innerHTML = '<i class="fa fa-spinner fa-pulse fa-3x fa-fw"></i>'
+    const response = await fetch('http://127.0.0.1:5000/refresh/token_holders', {
+        method: 'POST',
+        body: "",
+    })
+    if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+        document.getElementById("tokenHolders").innerHTML = data["token_holders"];
+        sessionStorage.setItem("tokenHolders", data["token_holders"]);
+    } else {
+        document.getElementById("tokenHolders").innerHTML = "Error querying API. Try again with Dashboard -> Refresh"
+        console.log(response);
+    }
+}
